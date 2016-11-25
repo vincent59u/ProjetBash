ab7c0b20cbdb61addfb4acd7963037ed4fb6e999
claurene <cladt.laurene@gmail.com>
2016-11-25 11:02:49 +0100

    Update backup.sh
    -vérification : si le dossier est déjà créé ou non
    -accès aux paramètres

--- sources/backup.sh
+++ sources/backup.sh
@@ -4,23 +4,28 @@
 # $j -> --backupdir /var/backups
 
 function backup(){
-	mkdir "$2" #vérifier si le dossier existe avant de le créer
+	if [ ! -d "$2" ]; then 
+		mkdir "$2" #créer le dossier $2 si il n'existe pas
+	fi 
 	cat "$1" | xargs -I{} cp {} $2 #attention, fichier .conf copié dans le nouveau dossier
 }
 
-if [ -z "$*" ]; then
+if [ -z "$*" ]; then #si aucun paramètre : noms par défault
 	backup backup.conf /var/backups
-else
-	for i in "$@"; do #changer en séquence
-		if [ "$i" == "--conf" ]; then
-			NOM = $(i+1)
-		elif [ "$i" == "--backupdir" ]; then
-			DIR = $(i+1)	
+else #sinon on récupère les noms du fichier et du dossier
+	NOM="backup.conf"
+	DIR="/var/backups"
+	for i in $(seq 1 $#); do 
+		j=$((i+1))		 
+		if [ "${!i}" == "--conf" ]; then
+			NOM=${!j}				
+		elif [ "${!i}" == "--backupdir" ]; then
+			DIR=${!j}	
 		fi
 	done
-	backup $NOM $DIR #créer variables nom et dir pour quand il n'y a rien
+	backup $NOM $DIR 
 fi
 
-#Commentaires : fonction non optimisée contre les bugs ( on peut mettre plusieurs fois ---conf, ce qui changera le nom etc. )
+#Commentaires : chercher les possibles bugs
 
 
