414ec14eaff9ec21bea9d5db333ceaaf6981f6d9
claurene <cladt.laurene@gmail.com>
2016-11-29 16:25:04 +0100

    Update compression.sh
    Englobé le code source dans une fonction compression()

--- sources/compression.sh
+++ sources/compression.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+function compression()
+{
+#à modifier : faire une boite de dialogue plutot qu'un echo 
 echo "Vous allez compresser $1"
 $(tar -cvf "$1.tar" $1 2>/dev/null) 2>/dev/null 
 $(gzip "$1.tar" ) 2>/dev/null
 $(rm "$1.tar" 2>/dev/null) 2>/dev/null
+}
