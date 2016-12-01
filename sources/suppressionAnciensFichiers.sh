8d98fb95c8aa376daa6b3d1fe678db9140ffc973
Matthieu <matthieu.vincent3@univ-lorraine.fr>
2016-12-01 20:27:00 +0100

    Ajoute le fichier qui permet la suppression des plus anciens fichiers du backup

---
+++ sources/suppressionAnciensFichiers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Programme qui permet de supprimer des anciens fichier dans le dossier de backup
+#Le dossier de backup conservera les 100 derniers fichiers du dossier
+function supprimerAnciensFichiers(){
+	#On compte (wc -l)  ici le nombre de fichier (param f) du dossier
+       	nbfichiers=$(find /var/backups -type f | wc -l )
+        #S'il y a plus de 100 fichiers dans le dossier
+        if [ $nbfichiers -gt 100 ]; then
+                #On cherche le plus ancien
+                suppr=$(ls /var/backups -tr1 | head -n 1)
+                #On le supprime
+                rm /var/backups/$suppr
+        fi
+}
