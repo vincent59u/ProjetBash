e8e95384e1df1177c1d6bfd91a28288691a830c7
claurene <cladt.laurene@gmail.com>
2016-12-08 21:04:31 +0100

    Correction de backup.sh et compression.sh pour avoir le bon dossier

--- SOURCES/backup.sh
+++ SOURCES/backup.sh
@@ -18,37 +18,40 @@ source GUI/fenetre.sh
 #######################################################################################################################################################
 function backup(){
 	#Appel d'une fonction qui permet à l utilisateur de saisir un nom de dossier dans une boite de dialogue
-        affiche_saisie "Saisir un nom de dossier" "Veulliez saisir le nom de dossier de votre backup."
-        #On teste que l'utilisateur à bien entré un nom de dossier et non un espace, une tabulation ou un retour à la ligne
+        affiche_saisie "Saisir un nom de dossier" "Veulliez saisir le nom de dossier de votre backup. (Laissez vide pour /var/backups)"
         retour=$?
-        while [[ -z "$saisie" && ! "$retour" -eq 1 ]]; do
-                affiche_saisie "Veuillez réessayer" "Entrez un nom de dossier de backup valide, c'est à dire avec des lettre et/ou des chiffres."
-                retour=$?
-        done
-        #Si l'utilisateur a appuyé sur annuler, on quitte la fonction de backup
-        if [ $retour -eq 1 ]; then
-                affiche_message "Annulation" "L'opération a bien été annulée"
-        else
-		#On crée le dossier qui contiendra les fichiers et les dossiers sauvegarder. Tous les backups auront un nom du même type (Backup + timestamp)
-		DIR=/var/backups/Backup_`date +%s`_"$saisie"
-		#créer un dossier de backup dans le répertoire /var/backup
-		mkdir $DIR
-		#On copie le dossier de backup l ensembles des fichier ou dossier indiqué dans le fichier de configuration
-		for ligne in $(cat $1); do
-			#Chiffrement de chaque fichiers avant de le copier dans le dossier de backup
-			chiffrement $ligne
-			#On déplace le fichier crypté dans le dossier de backup
-			mv ${ligne}.gpg $DIR
-		done
-	        #compression du dossier de backup. Une fois le dossier compressé, la version non-compressé sera supprimer par le programme.
-	        compression "$DIR"
-	        #On regarde si le nombre de backups est supérieur à 100. On supprime le plus ancien backup le cas échéant
-	        supprimerAnciensBackups
-	        #On indique à l'utilisateur si l opération s est bien déroulée ou non
-	        if [ $? -eq 0 ]; then
-	                affiche_message "Succès" "Le dossier de backup a été correctement fait."
-	        else
-	                affiche_message "Erreur..." "Une erreur est survenue lors de la création du dossier de backup, veuillez recommancer l'opération."
-	        fi
-	fi
+		#Si l'utilisateur a appuyé sur annuler, on quitte la fonction de backup
+		if [ $retour -eq 1 ]; then
+			affiche_message "Annulation" "L'opération a bien été annulée"
+		else
+			#Si le nom est vide, le dossier est /var/backups
+        	if [[ -z "$saisie" ]]; then
+            	saisie="/var/backups"
+       		else		    
+				#On crée le dossier qui contiendra le dossier de backups compressé, si ce dossier n'existe pas déjà
+				if [ ! -d "$saisie" ]; then 
+					mkdir $saisie
+				fi
+			fi			
+			#On créé le dossier de backup, qui contiendra les fichiers à sauvegarder et sera ensuite compressé
+			DIR=$saisie/`date +%s`
+			mkdir $DIR
+			#On copie le dossier de backup l ensembles des fichier ou dossier indiqué dans le fichier de configuration
+			for ligne in $(cat $1); do
+				#Chiffrement de chaque fichiers avant de le copier dans le dossier de backup
+				chiffrement $ligne
+				#On déplace le fichier crypté dans le dossier de backup
+				mv ${ligne}.gpg $DIR
+			done
+			    #compression du dossier de backup. Une fois le dossier compressé, la version non-compressé sera supprimée par le programme.
+			    compression "$DIR"
+			    #On regarde si le nombre de backups est supérieur à 100. On supprime le plus ancien backup le cas échéant
+			    supprimerAnciensBackups
+			    #On indique à l'utilisateur si l opération s est bien déroulée ou non
+			    if [ $? -eq 0 ]; then
+			            affiche_message "Succès" "Le dossier de backup a été correctement fait."
+			    else
+			            affiche_message "Erreur..." "Une erreur est survenue lors de la création du dossier de backup, veuillez recommancer l'opération."
+			    fi
+		fi
 }
--- SOURCES/compression.sh
+++ SOURCES/compression.sh
@@ -16,11 +16,13 @@ function compression(){
         else
 		if [ -d $1 ]; then
 			#Si le dossier existe, il est compressé
-			$(tar -cvf "$1.tar" $1 2>/dev/null) 2>/dev/null
-			$(gzip "$1.tar" 2>/dev/null) 2>/dev/null
+			cd $1/..
+			DIR_NAME=${1##*/}
+			$(tar -cvf "$DIR_NAME.tar" $DIR_NAME 2>/dev/null) 2>/dev/null
+			$(gzip "$DIR_NAME.tar" 2>/dev/null) 2>/dev/null
 			#Le dossier de backup est compressé en .tar.gz, on supprime donc le dossier .tar et le dossier non compressé
-			$(rm -Rf "$1.tar" 2>/dev/null) 2>/dev/null
-			$(rm -Rf "$1" 2>/dev/null) 2>/dev/null
+			$(rm -Rf "$DIR_NAME.tar" 2>/dev/null) 2>/dev/null
+			$(rm -Rf "$DIR_NAME" 2>/dev/null) 2>/dev/null
 		else
                         #Sinon on affiche le message d erreur concernant l inexistance du dossier
                         affiche_message "Erreur..." "Le dossier placé en paramètre n'existe pas"
