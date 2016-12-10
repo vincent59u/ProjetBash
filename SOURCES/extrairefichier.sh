fcf84f681a7c28cee8e48a5c0f4a19ac355c8c21
Matthieu <matthieu.vincent3@univ-lorraine.fr>
2016-12-10 10:24:56 +0100

    Ajoute des commentaire propre au fichiers de Benjamin

--- SOURCES/extrairefichier.sh
+++ SOURCES/extrairefichier.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
+
+# @author Laurene, Matthieu, Benjamin
+
+#Ce programme permet d extraire un fichier d une archive de backup.
+
+#######################################################################################################################################################
+#                                                               Importation des scripts utilisés                                                      #
+#######################################################################################################################################################
 source GUI/fenetre.sh
-#On spécifie le nom de l'archive et du fichier à extraire
-affiche_selectionFichier "Veuillez selectionner l'archive" "/var/backups"
-if [ $? -eq 0 ]; then
-#On récupère le chemin du dossier
-dossier=$(echo "$fichier" | grep -oEi "[0-9]*")
-#Choix du fichier à saisir
-affiche_saisie "Choisir fichier à extraire de $fichier" "Fichier"
-fich=$saisie
-#Si l'utilisateur a bien appuyé sur Accepter et non Annuler
-if [ $? -eq 0 ]; then
-	#On choisit la destination ( pour éviter qu'il ecrase un fichier existant )
-	affiche_saisie "Choisir chemin pour votre fichier" "Chemin"
-	#S'il a choisit Accepter
+
+#######################################################################################################################################################
+#                                                               Fonction d'extraction de fichier                                                      #
+#######################################################################################################################################################
+#Fonction qui permet d'extraire un fichier d une archive
+function extraireFichier(){
+	#On demande à l'utilisateur de choisir le dossier qui contient le fichier à extraire. Par défaut les backups se trouve dans le dossier /var/backups
+	affiche_selectionFichier "Veuillez selectionner un dossier contenant un backup" "/var/backups"
+	if [ $? -eq 0 ]; then
+		#On récupère le chemin du dossier
+		dossier=$(echo "$fichier" | grep -oEi "[0-9]*")
+		#Choix du fichier à saisir
+		affiche_saisie "Choisir fichier à extraire de $fichier" "Fichier"
+		fich=$saisie
+		#Si l'utilisateur a bien appuyé sur Accepter et non Annuler
 		if [ $? -eq 0 ]; then
-			destination=($saisie)
-			#On decompresse le fichier choisi de l'archive choisi dans la destination choisi ( Beaucoup de choisi n est pas ? )
-			`tar xfvz $fichier $dossier"/"$fich.gpg -C $destination`
+			#On choisit la destination ( pour éviter qu'il ecrase un fichier existant )
+			affiche_saisie "Choisir chemin pour votre fichier" "Chemin"
+			#S'il a choisit Accepter
+			if [ $? -eq 0 ]; then
+				destination=($saisie)
+				#On decompresse le fichier choisi de l'archive choisi dans la destination choisi ( Beaucoup de choisi n est pas ? )
+				`tar xfvz $fichier $dossier"/"$fich.gpg -C $destination`
+			fi
 		fi
 	fi
-fi
+}
--- SOURCES/synopsis.sh
+++ SOURCES/synopsis.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
-#Si l'utilisateur n a pas de dossier GoT on en crée un
-if [ ! -d "/home/user/GoT" ]; then
-	mkdir "/home/user/GoT"
-fi
-#IFS permet de changer le séparateur pour ne prendre en compte que les retour à la ligne et #pas les espaces
-IFS=$'\n'
-#La première regex sert à récupérer le fichier de tous les synopsis afin de boucler sur tous les détails de synopsis
-regex="<a href=\"synopsis\.php\?s=([0-9]{1,2})\&amp;e=[0-9]{1,2}\">Episode ([0-9]{1,2}): (([A-Za-z,]+ {0,1})+)<\/a>"
-#La seconde regex récupère le contenu de la balise avec le texte de la synopsis
-regex2="<p class=\"left-align light\">([a-zA-Z,.;']+ {0,1})+<\/p>"
-FILE=$(curl -O https://daenerys.xplod.fr/synopsis.php)
-#Pour chaque synopsis
-for titre in $(cat synopsis.php); do
-        if [[ "$titre" =~ $regex ]]; then
-		#On affiche le nom de l'épisode
-                echo "S"${BASH_REMATCH[1]}"E"${BASH_REMATCH[2]}":"${BASH_REMATCH[3]}
-		#On récupère la page de la synopsis avec Curl
-		`curl -o "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" https://daenerys.xplod.fr/synopsis.php?s=${BASH_REMATCH[1]}"&"e=${BASH_REMATCH[2]} | grep -oEi "$regex2"`
-		#On ne récupère que ce qui match la regex dans la variable valtemp
-		valtemp=`cat "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" | grep -oEi $regex2`
-		#On echo ensuite le texte sans les balises
-		`echo $valtemp | grep -oE "[A-Z]{1}([a-zA-Z,.;']+ {0,1})+">"/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
-		#Enfin on ajoute le lien vers le fichier supersynopsis
-		`echo "https://daenerys.xplod.fr/supsyn.php?e="${BASH_REMATCH[2]}"&s="${BASH_REMATCH[1]}>> "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
-		#On crée le fichier supersynopsis dans le dossier GoT
-		#`curl -o "/home/user/GoT/synopsis_"${BASH_REMATCH[1]}"_"${BASH_REMATCH[2]}".syn.gpg" https://daenerys.xplod.fr/supsyn.php?e=${BASH_REMATCH[2]}"&"s=${BASH_REMATCH[1]}`
-        fi
-done
-#On supprime le fichier permettant d'afficher le nom des épisodes
-rm synopsis.php
+
+# @author Laurene, Matthieu, Benjamin
+
+#Programme de récupération de synopsis.
+#Il permet de récupérer les synopsys de la série télévisée Games of Thrones
+
+#######################################################################################################################################################
+#								Fonction de récupération de synopsis						      #
+#######################################################################################################################################################
+#Fonction de récupération de synopsis sur un serveur distant
+function recupererSynopsis(){
+	#Si l'utilisateur n a pas de dossier GoT on en crée un
+	if [ ! -d "/home/user/GoT" ]; then
+		mkdir "/home/user/GoT"
+	fi
+	#IFS permet de changer le séparateur pour ne prendre en compte que les retour à la ligne et #pas les espaces
+	IFS=$'\n'
+	#La première regex sert à récupérer le fichier de tous les synopsis afin de boucler sur tous les détails de synopsis
+	regex="<a href=\"synopsis\.php\?s=([0-9]{1,2})\&amp;e=[0-9]{1,2}\">Episode ([0-9]{1,2}): (([A-Za-z,]+ {0,1})+)<\/a>"
+	#La seconde regex récupère le contenu de la balise avec le texte de la synopsis
+	regex2="<p class=\"left-align light\">([a-zA-Z,.;']+ {0,1})+<\/p>"
+	FILE=$(curl -O https://daenerys.xplod.fr/synopsis.php)
+	#Pour chaque synopsis
+	for titre in $(cat synopsis.php); do
+	        if [[ "$titre" =~ $regex ]]; then
+			#On affiche le nom de l'épisode
+	                echo "S"${BASH_REMATCH[1]}"E"${BASH_REMATCH[2]}":"${BASH_REMATCH[3]}
+			#On récupère la page de la synopsis avec Curl
+			`curl -o "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" https://daenerys.xplod.fr/synopsis.php?s=${BASH_REMATCH[1]}"&"e=${BASH_REMATCH[2]} | grep -oEi "$regex2"`
+			#On ne récupère que ce qui match la regex dans la variable valtemp
+			valtemp=`cat "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" | grep -oEi $regex2`
+			#On echo ensuite le texte sans les balises
+			`echo $valtemp | grep -oE "[A-Z]{1}([a-zA-Z,.;']+ {0,1})+">"/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
+			#Enfin on ajoute le lien vers le fichier supersynopsis
+			`echo "https://daenerys.xplod.fr/supsyn.php?e="${BASH_REMATCH[2]}"&s="${BASH_REMATCH[1]}>> "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
+			#On crée le fichier supersynopsis dans le dossier GoT
+			#`curl -o "/home/user/GoT/synopsis_"${BASH_REMATCH[1]}"_"${BASH_REMATCH[2]}".syn.gpg" https://daenerys.xplod.fr/supsyn.php?e=${BASH_REMATCH[2]}"&"s=${BASH_REMATCH[1]}`
+	        fi
+	done
+	#On supprime le fichier permettant d'afficher le nom des épisodes
+	rm synopsis.php
+}
