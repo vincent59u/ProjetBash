f80097ccd23428bfbfc7fa89a9c0f50544adfc9a
Matthieu <matthieu.vincent3@univ-lorraine.fr>
2016-12-10 11:11:19 +0100

    Modifie le chemin du dossier GoT pour qu'il fonctionne sur n'importe quelle machine

--- SOURCES/synopsis.sh
+++ SOURCES/synopsis.sh
@@ -11,8 +11,8 @@
 #Fonction de récupération de synopsis sur un serveur distant
 function recupererSynopsis(){
 	#Si l'utilisateur n a pas de dossier GoT on en crée un
-	if [ ! -d "/home/user/GoT" ]; then
-		mkdir "/home/user/GoT"
+	if [ ! -d "$HOME/GoT" ]; then
+		mkdir "$HOME/GoT"
 	fi
 	#IFS permet de changer le séparateur pour ne prendre en compte que les retour à la ligne et #pas les espaces
 	IFS=$'\n'
@@ -27,15 +27,15 @@ function recupererSynopsis(){
 			#On affiche le nom de l'épisode
 	                echo "S"${BASH_REMATCH[1]}"E"${BASH_REMATCH[2]}":"${BASH_REMATCH[3]}
 			#On récupère la page de la synopsis avec Curl
-			`curl -o "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" https://daenerys.xplod.fr/synopsis.php?s=${BASH_REMATCH[1]}"&"e=${BASH_REMATCH[2]} | grep -oEi "$regex2"`
+			`curl -o "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" https://daenerys.xplod.fr/synopsis.php?s=${BASH_REMATCH[1]}"&"e=${BASH_REMATCH[2]} | grep -oEi "$regex2"`
 			#On ne récupère que ce qui match la regex dans la variable valtemp
-			valtemp=`cat "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" | grep -oEi $regex2`
+			valtemp=`cat "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" | grep -oEi $regex2`
 			#On echo ensuite le texte sans les balises
-			`echo $valtemp | grep -oE "[A-Z]{1}([a-zA-Z,.;']+ {0,1})+">"/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
+			`echo $valtemp | grep -oE "[A-Z]{1}([a-zA-Z,.;']+ {0,1})+">"$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
 			#Enfin on ajoute le lien vers le fichier supersynopsis
-			`echo "https://daenerys.xplod.fr/supsyn.php?e="${BASH_REMATCH[2]}"&s="${BASH_REMATCH[1]}>> "/home/user/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
+			`echo "https://daenerys.xplod.fr/supsyn.php?e="${BASH_REMATCH[2]}"&s="${BASH_REMATCH[1]}>> "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
 			#On crée le fichier supersynopsis dans le dossier GoT
-			#`curl -o "/home/user/GoT/synopsis_"${BASH_REMATCH[1]}"_"${BASH_REMATCH[2]}".syn.gpg" https://daenerys.xplod.fr/supsyn.php?e=${BASH_REMATCH[2]}"&"s=${BASH_REMATCH[1]}`
+			#`curl -o "$HOME/GoT/synopsis_"${BASH_REMATCH[1]}"_"${BASH_REMATCH[2]}".syn.gpg" https://daenerys.xplod.fr/supsyn.php?e=${BASH_REMATCH[2]}"&"s=${BASH_REMATCH[1]}`
 	        fi
 	done
 	#On supprime le fichier permettant d'afficher le nom des épisodes
