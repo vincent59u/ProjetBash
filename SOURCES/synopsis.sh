28d6fd0b5cda852f32410bfc50a1053945789dd3
claurene <cladt.laurene@gmail.com>
2016-12-12 14:43:33 +0100

    Ajout de la vérification de la signature d'un synopsis ; Seul les synopsis valides sont récupérés //TODO : Gérer les erreurs de connexion + envoi de notifications par email

--- SOURCES/synopsis.sh
+++ SOURCES/synopsis.sh
@@ -8,6 +8,9 @@
 #######################################################################################################################################################
 #								Fonction de récupération de synopsis						      #
 #######################################################################################################################################################
+
+#//TODO : si le script fonctionne en silencieux, les notifications doivent être envoyées par mail (récupèrer le mail dans la clé publique ?)
+
 #Fonction de récupération de synopsis sur un serveur distant
 function recupererSynopsis(){
 	#Si l'utilisateur n a pas de dossier GoT on en crée un
@@ -21,23 +24,50 @@ function recupererSynopsis(){
 	#La seconde regex récupère le contenu de la balise avec le texte de la synopsis
 	regex2="<p class=\"left-align light\">([a-zA-Z,.;']+ {0,1})+<\/p>"
 	FILE=$(curl -O https://daenerys.xplod.fr/synopsis.php)
+	#On va récupèrer chaque SuperSynopsis, et récupérer les synopsis valides
+	#On récupère la clé publique disponible sur le site
+	curl https://daenerys.xplod.fr/supersynopsis_signature.pub ​> public.key
+	#On importe la clé récupérée dans notre trousseau
+	gpg --import public.key
+	#On supprime ensuite le fichier temporaire
+	rm public.key
+	#On récupère le chemin des fichiers à vérifier
 	#Pour chaque synopsis
 	for titre in $(cat synopsis.php); do
 	        if [[ "$titre" =~ $regex ]]; then
-			#On affiche le nom de l'épisode
-	                echo "S"${BASH_REMATCH[1]}"E"${BASH_REMATCH[2]}":"${BASH_REMATCH[3]}
-			#On récupère la page de la synopsis avec Curl
-			`curl -o "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" https://daenerys.xplod.fr/synopsis.php?s=${BASH_REMATCH[1]}"&"e=${BASH_REMATCH[2]} | grep -oEi "$regex2"`
-			#On ne récupère que ce qui match la regex dans la variable valtemp
-			valtemp=`cat "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" | grep -oEi $regex2`
-			#On echo ensuite le texte sans les balises
-			`echo $valtemp | grep -oE "[A-Z]{1}([a-zA-Z,.;']+ {0,1})+">"$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
-			#Enfin on ajoute le lien vers le fichier supersynopsis
-			`echo "https://daenerys.xplod.fr/supsyn.php?e="${BASH_REMATCH[2]}"&s="${BASH_REMATCH[1]}>> "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
-			#On crée le fichier supersynopsis dans le dossier GoT
-			#`curl -o "$HOME/GoT/synopsis_"${BASH_REMATCH[1]}"_"${BASH_REMATCH[2]}".syn.gpg" https://daenerys.xplod.fr/supsyn.php?e=${BASH_REMATCH[2]}"&"s=${BASH_REMATCH[1]}`
+				#On crée le fichier supersynopsis dans le dossier GoT
+				`curl -o "$HOME/GoT/synopsis_"${BASH_REMATCH[1]}"_"${BASH_REMATCH[2]}".syn.gpg" https://daenerys.xplod.fr/supsyn.php?e=${BASH_REMATCH[2]}"&"s=${BASH_REMATCH[1]}`
+				FILE="$HOME/GoT/synopsis_"${BASH_REMATCH[1]}"_"${BASH_REMATCH[2]}".syn.gpg"
+				if `checkFile $FILE`; then
+					#On récupère la page de la synopsis avec Curl
+					`curl -o "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" https://daenerys.xplod.fr/synopsis.php?s=${BASH_REMATCH[1]}"&"e=${BASH_REMATCH[2]} | grep -oEi "$regex2"`
+					#On ne récupère que ce qui match la regex dans la variable valtemp
+					valtemp=`cat "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt" | grep -oEi $regex2`
+					#On echo ensuite le texte sans les balises
+					`echo $valtemp | grep -oE "[A-Z]{1}([a-zA-Z,.;']+ {0,1})+">"$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
+					#Enfin on ajoute le lien vers le fichier supersynopsis
+					`echo "https://daenerys.xplod.fr/supsyn.php?e="${BASH_REMATCH[2]}"&s="${BASH_REMATCH[1]}>> "$HOME/GoT/Saison "${BASH_REMATCH[1]}" Episode  "${BASH_REMATCH[2]}".txt"`
+				fi
+			#On supprime le fichier SuperSynopsis
+			rm $FILE
 	        fi
 	done
 	#On supprime le fichier permettant d'afficher le nom des épisodes
 	rm synopsis.php
 }
+
+#Fonction permettant de vérifier si un fichier de synopsis passé en paramètre est correct ; pour se faire, on vérifie sa signature
+checkFile(){
+	gpg --verify "$1" >&- 2>&-
+	#Code de sortie : 0 -> ok, >0 -> pas ok
+	RESULT=$?
+	if [[ "$RESULT" -eq 0 ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+#On teste la fonction pour voir si elle fonctionne (et elle fonctionne bien)
+recupererSynopsis
+
