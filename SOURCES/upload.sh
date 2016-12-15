0c250178bbb1f660858bd0f9032b78c7e0ede180
Benjamin Rath <benjamin.rath@cegetel.net>
2016-12-15 01:13:58 +0100

    Mise a jour de upload permettant de lister et récupération de tout les fichiers et hashs présents sur le serveur
    Il faut ensuite parcourir chaque fichier et les associer un a un aux
    hashs et faire un selecteur a double entrée pour que l'utilisateur
    puisse choisir quel fichier il veut récup
    
    Par contre il est possible d'utiliser lister pour avoir les fichiers et
    ne plus avoir besoin du fichier ! A VOIR

--- SOURCES/upload.sh
+++ SOURCES/upload.sh
@@ -23,9 +23,10 @@ if [[ "$fichier" =~ '.tar.gz'$ ]]; then
 		#On enlève le = devant
 		hash=$(echo $hash | grep -oE "[a-z0-9]+")
 		#On met le login dans le fichier de save
-		echo "Cladt_Rath_Vincent" >> "/home/user/hashsauvegarde.txt"
+		echo "#Cladt_Rath_Vincent#" >> "$HOME/hashsauvegarde.txt"
+		echo "($fichier)" >> "$HOME/hashsauvegarde.txt"
 		#On met le hash dans le fichier de save
-		echo $hash >> "/home/user/hashsauvegarde.txt"
+		echo $hash >> "$HOME/hashsauvegarde.txt"
 		affiche_message "Fichier uploadé avec succés" "Login : Cladt_Rath_Vincent \n Hash = $hash\n Recapitulatif de vos backups dans /home/user/hashsauvegarde.txt"
 	else
 		affiche_message "Problème avec Curl" "Erreur avec Curl"
@@ -37,5 +38,19 @@ fi
 #Utile pour la récup
 #curl https://daenerys.xplod.fr/backup/list.php?login=Cladt_Rath_Vincent
 
+function listerBackup(){
+curl https://daenerys.xplod.fr/backup/list.php?login=Cladt_Rath_Vincent
+}
 
+function downloadBackup(){
+#IFS=$'\n'
+regexhash="^[a-z0-9]+$"
+regexfichier="\([A-Za-z0-9\/\.]+\)"
+fichiersgz=$(cat "$HOME/hashsauvegarde.txt" | grep -oE $regexfichier)
+fichiersgz=$(echo $fichiersgz | grep -oE "[0-9]+\.tar\.gz")
+hashs=$(cat "$HOME/hashsauvegarde.txt" | grep -oE $regexhash)
+echo $fichiersgz  #Le noms des fichiers présents sur daenerys
+echo $hashs #La liste de tout les hashs présents sur le serveur daenerys
+
+}
 
